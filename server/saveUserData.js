79b970aef95945e4003210c677ac020402570108
Oscar Shaw <oscar@acutulus.co>
2018-06-09 20:03:25 -0700

    Callback endpoints

---
+++ server/saveUserData.js
@@ -0,0 +1,6 @@
+module.exports = (callback) => {
+  // Save user data to database
+  // Create DocuSign template
+  // Send template to DocuSign to send email requesting signature to user
+  callback('saveUserData response');
+}
